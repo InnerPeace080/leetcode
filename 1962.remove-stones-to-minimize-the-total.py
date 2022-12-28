05e4d74bec8fd76f9434a27251a15392055231eb
phuongtq <tranphuong.080@gmail.com>
2022-12-28 17:29:49 +0700

    add: 1962

---
+++ 1962.remove-stones-to-minimize-the-total.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1962 lang=python3
+#
+# [1962] Remove Stones to Minimize the Total
+#
+
+# @lc code=start
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        
+# @lc code=end
+

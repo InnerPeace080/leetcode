b7bff5a167259577b44a817cbb09434152ea6258
phuongtq <tranphuong.080@gmail.com>
2022-12-29 10:53:11 +0700

    add: 1834

---
+++ 1834.single-threaded-cpu.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode id=1834 lang=python3
+#
+# [1834] Single-Threaded CPU
+#
+
+# @lc code=start
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        pass
+# @lc code=end

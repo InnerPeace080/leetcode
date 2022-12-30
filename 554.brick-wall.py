7736309bdc5a2c07608dddaac863aee5750cb600
phuongtq <tranphuong.080@gmail.com>
2022-12-30 14:44:41 +0700

    add(554):

---
+++ 554.brick-wall.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=554 lang=python3
+#
+# [554] Brick Wall
+#
+
+import heapq
+import inspect
+import json
+from collections import deque
+from typing import List
+
+from timebudget import timebudget
+
+
+# @lc code=start
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        return
+# @lc code=end

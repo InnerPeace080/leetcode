fda4aeefd7b6bd36439a5a214c484dd46171f575
phuongtq <tranphuong.080@gmail.com>
2022-12-30 15:21:05 +0700

    add(560)

---
+++ 560.subarray-sum-equals-k.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=560 lang=python3
+#
+# [560] Subarray Sum Equals K
+#
+
+import heapq
+import inspect
+import json
+from collections import deque
+from typing import List
+
+
+# @lc code=start
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        return
+# @lc code=end

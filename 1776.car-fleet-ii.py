58999326717fc42f2469d677ff6e6efbe6b28d16
tranphuong.080 <tranphuong.080@gmail.com>
2023-01-02 17:11:07 +0700

    add(1776)

---
+++ 1776.car-fleet-ii.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1776 lang=python3
+#
+# [1776] Car Fleet II
+#
+
+import heapq
+import inspect
+import json
+from collections import deque
+from typing import List
+
+
+# @lc code=start
+class Solution:
+    def getCollisionTimes(self, cars: List[List[int]]) -> List[float]:
+        pass
+# @lc code=end
